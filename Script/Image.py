ec16b3de7bdfa1c8922cc01847d934696f19381b
Blankuca <blancarobledodiaz@gmail.com>
2020-03-22 19:13:41 +0100

    Update

--- Script/Image.py
+++ Script/Image.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import rasterio
 import numpy as np
+import matplotlib.image as mpimg
 
 #%% Create dataframe of image with band values and include water indexes.
 def bands_to_df(raster):
@@ -79,8 +80,68 @@ def RGB(rasterpath):
     G = raster.read(3)
     B = raster.read(2)
     plt.imshow(R+G+B)
-      
+    
 def RGB_hist(df):
     df['B1'].hist(bins=50, alpha= 0.3,color = 'red')
     df['B2'].hist(bins=50, alpha= 0.3,color = 'green')
     df['B3'].hist(bins=50, alpha= 0.3,color = 'blue')
+    
+
+def RGB_vs_img(df, path, date):
+    plt.figure(figsize=(10,7))
+
+    plt.subplot(2, 2, 1) 
+    plt.title('Spectral signature for '+date)
+    axes = plt.gca()
+    axes.set_xlim([0,2500])
+    RGB_hist(df)
+
+    plt.subplot(2, 2, 2)
+    plt.title('RGB for '+date)
+    img=mpimg.imread(path)
+    imgplot = plt.imshow(img)
+    
+def class_spectral_signature(training):
+    plt.figure(figsize=(10,8))
+
+    plt.subplot(2, 2, 1) 
+    plt.title('Spectral signature for Large Water Bodies')
+    axes = plt.gca()
+    axes.set_xlim([-1000,3000])
+    RGB_hist(training.loc[training['Content'] == 1])
+
+    plt.subplot(2, 2, 2) 
+    plt.title('Spectral signature for Small Water Bodies')
+    axes = plt.gca()
+    axes.set_xlim([-1000,3000])
+    RGB_hist(training.loc[training['Content'] == 3])
+
+    plt.subplot(2, 2, 3) 
+    plt.title('Spectral signature for Non Water areas')
+    axes = plt.gca()
+    axes.set_xlim([-1000,3000])
+    RGB_hist(training.loc[training['Content'] == 2])
+    
+    plt.show()
+    
+def class_piechart(training):
+    
+    lwb = training.loc[training['Content'] == 1]
+    swb = training.loc[training['Content'] == 2]
+    nw = training.loc[training['Content'] == 3]
+
+    n_lwb = len(lwb); n_swb = len(swb); n_nw = len(nw);
+
+    # Data to plot
+    labels = ['Large Water Bodies', 'Small Water Bodies', 'Non Water']
+    sizes = [n_lwb,n_swb,n_nw]
+    colors = ['dodgerblue', 'goldenrod', 'forestgreen']
+
+    # Plot
+    plt.pie(sizes, labels=labels, colors=colors,
+    autopct='%1.1f%%', shadow=True, startangle=140)
+
+    plt.axis('equal')
+    plt.show()
+    
+    return n_lwb, n_nw, n_swb
--- Script/Validation.py
+++ Script/Validation.py
@@ -11,6 +11,7 @@ from rasterio.mask import mask
 import geopandas as gpd
 import pandas as pd
 
+# Given a raster, put the bands into a pandas dataframe.
 def vector_to_df(raster):
     df = pd.DataFrame()
     i = 0
@@ -21,33 +22,36 @@ def vector_to_df(raster):
     
     return df 
 
-def shapes_to_dataset(waterpath, rasterpath):
+# Given the paths of a shapefile and a GeoTiff file,
+# clip the raster and put it into a dataframe.
+def shapes_to_dataset(shapepath, rasterpath):
     
     raster = rasterio.open(rasterpath)
-    vector = gpd.read_file(waterpath)
+    vector = gpd.read_file(shapepath)
     
-    out_img, out_transform = mask(raster, vector['geometry'], crop=True)
+    out_img, _ = mask(raster, vector['geometry'], crop=True)
     
     df_shp = vector_to_df(out_img)
     
-    dim = out_img[0].shape
+    return df_shp
+
+# Given three shapefiles and a GeoTiff files, return a dataframe with 
+# the band values and the class of every pixel belonging to the shapefiles.
+def make_dataset(waterpath, groundpath, riverpath, rasterpath):
     
-    return {'dim':dim, 'df':df_shp}
-    
-def make_dataset(waterpath, groundpath, rasterpath):
-    
-    water_dict = shapes_to_dataset(waterpath, rasterpath)
-    ground_dict = shapes_to_dataset(groundpath, rasterpath)
-    
-    water = water_dict['df']
-    ground = ground_dict['df']
+    bands = ['B1','B2','B3','B4','B5','B6','B7','B8','B9','B10','B11','B12','B13']
+    water = shapes_to_dataset(waterpath, rasterpath)[bands]
+    river = shapes_to_dataset(riverpath, rasterpath)[bands]
+    ground = shapes_to_dataset(groundpath, rasterpath)[bands]
     
     water['Content'] = [1]*len(water)
-    ground['Content'] = [0]*len(ground)
+    river['Content'] = [2]*len(river)
+    ground['Content'] = [3]*len(ground)
     
     ground = ground[(ground['B1'] != 0)]
     water = water[(water['B1'] != 0)]
+    river = river[(river['B1'] != 0)]
     
-    df = pd.concat([water, ground])
+    df = pd.concat([water, ground, river])
     
-    return {'df':df,'dim_water':water_dict['dim'],'dim_ground':ground_dict['dim']}
+    return df
